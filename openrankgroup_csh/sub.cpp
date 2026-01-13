2a02a4ab14868e6f8f7bb1400ffa451bd3603e79
函数差 <2593596148@qq.com>
2026-01-14 01:04:13 +0800

    Delete openrankgroup_csh directory

--- openrankgroup_csh/sub.cpp
+++
@@ -1,5 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    printf("2-1=%d\n",2-1);
-}
--- openrankgroup_csh/sum.cpp
+++
@@ -1,5 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    printf("1+1=%d\n",1+1);
-}
