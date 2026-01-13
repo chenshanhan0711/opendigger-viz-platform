9429c395b8e1ac9db6165007500cac8eb79f4c43
函数差 <2593596148@qq.com>
2026-01-14 00:09:40 +0800

    Add files via upload

---
+++ openrankgroup_csh/sub.cpp
@@ -0,0 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    printf("2-1=%d\n",2-1);
+}
---
+++ openrankgroup_csh/sum.cpp
@@ -0,0 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    printf("1+1=%d\n",1+1);
+}
